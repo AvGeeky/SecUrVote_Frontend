cca75d9b457933dd54617f613d1c99808f2626ba
AvGeeky <sai.saip2005@gmail.com>
2025-02-01 00:42:40 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/securvotepro",
-})
+  base: '/securvotepro/', // Set the base path for the app if it's hosted on a subdirectory
+});
